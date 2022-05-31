56b42b8720f62ebc50c0dd1bdf550d638abc56bf
sammy899 <105850570+sammy899@users.noreply.github.com>
2022-05-31 14:47:00 +0530

    Add files via upload

---
+++ Methodoverloading.java
@@ -0,0 +1,44 @@
+package samiksha;
+
+public class Methodoverloading {
+
+	
+	public static void methodOverloaded()
+	    {
+	        
+	    }
+	 
+	    void methodOverloaded(int i)
+	    {
+	        
+	    }
+	 
+	    void methodOverloaded(double d)
+	    {
+	      
+	    }
+	 
+	    void methodOverloaded(int i, double d)
+	    {
+	    	
+	    }
+	 
+	    void methodOverloading(double d, int i)
+	    {
+	        
+	    }
+	 
+	    void methodOverloaded(double d1, int i1)
+	    {
+	                
+	    }
+	 
+	    void differentMethod()
+	    {
+	        //Different method
+	    }
+	
+
+	}
+
+
---
+++ Thiskeyword.java
@@ -0,0 +1,5 @@
+package samiksha;
+
+public class Thiskeyword {
+
+}
